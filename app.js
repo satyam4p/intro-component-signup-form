181f6213899171e954e1c168fb2bde7fbc4c68c6
Satyam Kumar <satyamkumar343@gmail.com>
2021-05-23 23:23:50 +0530

    second

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
-
 function handleSubmit(){
     event.preventDefault();
-    console.log(event);
     const firstName = event.target.firstName.value;
     const lastName = event.target.lastName.value;
     const email = event.target.email.value;
@@ -14,11 +12,9 @@ function handleSubmit(){
     if(validateInput(inputDetails,event.target)){
         event.target.submit();
         window.location.search="";
-        
     }
 }
 
-console.log();
 function handleFirstName(){
     event.target.value === " " || event.target.value==="" ? 
     raiseWarning(event.target,"First name cannot be empty"):callSuccess(event.target);
@@ -50,18 +46,17 @@ function handlePassword(){
 }
 
 function validateInput(inputObj,target){
-    console.log(inputObj);
     
     let fn=true,ln=true,em=true,pw=true;
     if(inputObj.firstName ==="") {
         raiseWarning(target.firstName,"First name cannot be empty");
         fn=false;
     }
-    if(inputObj.lastName ==="") {
+    if(inputObj.lastName === "") {
         raiseWarning(target.lastName,"Last name cannot be empty");
         ln = false;
     }
-    if(inputObj.email ==="") {
+    if(inputObj.email === "") {
         raiseWarning(target.email,"Email cannot be empty");
         em=false;
     }else if( !validateEmail(inputObj.email)){
@@ -88,8 +83,6 @@ function raiseWarning(input, message) {
     icon.style.opacity = 1;
     input.classList = "input-error";
 }
-
-
 
 function callSuccess(input) {
     const form = input.parentElement;
